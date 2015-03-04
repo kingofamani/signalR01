11e74cb2a16c292a2178aa0fcc6550ba2df7e043
AMANI <kingofamani@gmail.com>
2015-03-04 14:26:19 +0800

    init

---
+++ SampleSR/App_Code/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+public class ChatHub : Hub
+{
+    //Server端供Client呼叫的方法
+    public void Send(string message)
+    {
+        //Server端呼叫Client端
+        Clients.All.show(message);
+    }       
+}
